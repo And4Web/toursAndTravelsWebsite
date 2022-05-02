a4276f4c202c7be418c5ed4ebf7c763b78f156e1
And4Web <anands2807@gmail.com>
2022-05-02 10:13:20 +0530

    login form transition added for smooth in and out

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 let searchBtn = document.getElementById("search-btn");
-
 let searchBar = document.querySelector(".search-bar-container");
+
+let loginBtn = document.getElementById("login-btn");
+let formClose = document.getElementById("form-close");
+let loginForm = document.querySelector(".login-form-container");
 
 window.onscroll = () => {
   searchBtn.classList.remove("fa-times");
@@ -10,4 +13,11 @@ window.onscroll = () => {
 searchBtn.addEventListener("click", () => {
   searchBtn.classList.toggle("fa-times");
   searchBar.classList.toggle("active");
+});
+
+loginBtn.addEventListener("click", () => {
+  loginForm.classList.add("active");
+});
+formClose.addEventListener("click", () => {
+  loginForm.classList.remove("active");
 });
