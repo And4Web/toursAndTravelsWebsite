af9cf0a14faf4073ff7542c5e1369b56014bfaec
And4Web <anands2807@gmail.com>
2022-05-02 19:49:55 +0530

    responsive menubar created

--- js/script.js
+++ js/script.js
@@ -5,9 +5,14 @@ let loginBtn = document.getElementById("login-btn");
 let formClose = document.getElementById("form-close");
 let loginForm = document.querySelector(".login-form-container");
 
+let menuBtn = document.getElementById("menu-bar");
+let navBar = document.querySelector(".navbar");
+
 window.onscroll = () => {
   searchBtn.classList.remove("fa-times");
   searchBar.classList.remove("active");
+  menuBtn.classList.remove("fa-times");
+  navBar.classList.remove("active");
 };
 
 searchBtn.addEventListener("click", () => {
@@ -20,4 +25,9 @@ loginBtn.addEventListener("click", () => {
 });
 formClose.addEventListener("click", () => {
   loginForm.classList.remove("active");
+});
+
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("fa-times");
+  navBar.classList.toggle("active");
 });
