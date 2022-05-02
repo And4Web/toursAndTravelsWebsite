e51bd545e29aa3b3db80d5bf5b465674082b73a5
And4Web <anands2807@gmail.com>
2022-05-02 21:04:33 +0530

    home section with video slider created.

--- js/script.js
+++ js/script.js
@@ -8,6 +8,8 @@ let loginForm = document.querySelector(".login-form-container");
 let menuBtn = document.getElementById("menu-bar");
 let navBar = document.querySelector(".navbar");
 
+let videoBtn = document.querySelectorAll(".vid-btn");
+
 window.onscroll = () => {
   searchBtn.classList.remove("fa-times");
   searchBar.classList.remove("active");
@@ -30,4 +32,13 @@ formClose.addEventListener("click", () => {
 menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("fa-times");
   navBar.classList.toggle("active");
+});
+
+videoBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document.querySelector(".controls .active").classList.remove("active");
+    btn.classList.add("active");
+    let src = btn.getAttribute("data-src");
+    document.querySelector("#video-slider").src = src;
+  });
 });
